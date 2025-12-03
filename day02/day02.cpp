3253146a2adab946693dadf85f8bb16ff8a75d51
Ruslan Karabalin <karabalin112@gmail.com>
2025-12-03 17:33:16 +0300

    feat: add day02 solution

---
+++ day02/day02.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <string>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cout.tie(nullptr);
+    std::cin.tie(nullptr);
+
+    unsigned long long sum = 0;
+    std::string line;
+    while(std::getline(std::cin, line, ',')) {
+        std::string::size_type d = line.find('-');
+        unsigned long long start = std::stoll(line.substr(0, d));
+        unsigned long long end = std::stoll(line.substr(d + 1, line.length()));
+        for (long long i = start; i <= end; ++i) {
+            std::string num = std::to_string(i);
+            if (num.length() % 2 == 0) {
+                long long partLen = num.length() / 2;
+                std::string firstPart = num.substr(0, partLen);
+                std::string secondPart = num.substr(partLen, num.length());
+                if (firstPart == secondPart) {
+                    sum += i;
+                }
+            }
+        }
+    }
+    std::cout << sum << std::endl;
+    return 0;
+}
